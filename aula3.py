7e7af86c32dba71e54893f940d7fa15bd95936ff
Pessinaaa <pedro.pessina99@gmail.com>
2024-08-26 09:52:10 -0300

    aula 3

---
+++ aula3.py
@@ -0,0 +1,13 @@
+print("Teste aula 1")
+
+n1 = 10
+n2 = 20
+
+total = n1 + n2
+
+print("O total é: ", total)
+
+if n1 > n2:
+    print("O valor de N1 é maior que N2")
+else:
+    print("O valor de N2 é maior que N1")
